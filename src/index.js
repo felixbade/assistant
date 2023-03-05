8b94babd47ebbf0c49dc0c8d8b7d0cda4828ab58
Felix Bade <flix.bade@gmail.com>
2023-03-05 20:44:48 +0200

    Arrow function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style/main.css'
 import './js/app.js'
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope)
-    }, (error) => {
+    }, error => {
         console.log('ServiceWorker registration failed: ', error)
     })
 }
