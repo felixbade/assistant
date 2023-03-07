b7febaf6c661d445d28774ec1d1d12dd41cb42e6
Felix Bade <flix.bade@gmail.com>
2023-03-07 23:00:55 +0200

    Fix arrow function syntax

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,40 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env) => {
-    entry: './src/index.js',
-    output: {
-        filename: 'main-[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: false, // do not remove type="text"
-            },
-            version: env.version,
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style-[contenthash].css',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/static', to: '' },
-            ],
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main-[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeRedundantAttributes: false, // do not remove type="text"
+                },
+                version: env.version,
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style-[contenthash].css',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'src/static', to: '' },
+                ],
+            }),
         ],
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
+            ],
+        },
+    }
 };
