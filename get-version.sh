25ed8e68d63eac7facc3c37c3a4788f5d3ef0a3f
Felix Bade <flix.bade@gmail.com>
2023-03-20 13:52:02 +0200

    Strip v from version tags, add * if uncommitted changes

--- get-version.sh
+++ get-version.sh
@@ -1,3 +1,16 @@
 # Return the current git tag (if present) or the first characters of the commit hash
 GIT_HASH=$(git log -n1 --pretty='%h')
-git describe --exact-match --tags $GIT_HASH 2> /dev/null || echo $GIT_HASH
+DESCRIBE=$(git describe --exact-match --tags $GIT_HASH 2> /dev/null)
+
+if [[ -n $DESCRIBE ]]; then
+    # Remove 'v' from beginning of tag, if present before a number
+    RESULT=$(echo $DESCRIBE | sed 's/^v\([0-9]\)/\1/')
+else
+    RESULT=$GIT_HASH
+    # Add a * to the hash if there are uncommitted changes
+    if [[ -n $(git status -s) ]]; then
+        RESULT+="*"
+    fi
+fi
+
+echo $RESULT
